c8edaf4b06e37be5f40c64ef83bc4eb41c08c866
Kirillishko <shumaev.kirill@gmail.com>
2023-02-05 23:50:49 +0500

    7

--- src/components/AnimeCompilation.tsx
+++ src/components/AnimeCompilation.tsx
@@ -2,20 +2,23 @@ import React, {FC} from 'react';
 import {IAnime} from "../models/Anime/IAnime";
 import "../styles/animeMain.css"
 import AnimeView from "./AnimeView";
+import {UseQuery} from "@reduxjs/toolkit/dist/query/react/buildHooks";
+import {IPagination} from "../models/IPagination";
+import {QueryDefinition} from "@reduxjs/toolkit/query";
+import {IAnimeData} from "../models/Anime/IAnimeData";
 
 interface AnimeCompilationProps {
-    animeCompilation: IAnime[],
-    title: string,
+    fetchAnime: UseQuery<QueryDefinition<IPagination | number, any, any, IAnimeData>>,
+    params : IPagination | number,
 }
 
-const AnimeCompilation: FC<AnimeCompilationProps> = ({animeCompilation, title}) => {
+const AnimeCompilation: FC<AnimeCompilationProps> = ({fetchAnime, params}) => {
+
+    const {data: animeData, error: error, isLoading: isLoading} = fetchAnime(params)
 
     return (
         <div className="animeCompilation">
-            <div className="animeCompilationTitle">
-                <h1>{title}</h1>
-            </div>
-            {animeCompilation.map(anime =>
+            {animeData && animeData.data.map(anime =>
                 <AnimeView key={anime.id} anime={anime}/>
             )}
         </div>
