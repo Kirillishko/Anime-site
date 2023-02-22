1953f610556e319c61140ddfdd6dd921477c93b1
Kirillishko <97043155+Kirillishko@users.noreply.github.com>
2023-02-22 15:18:07 +0500

    Update SearchPage.tsx

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,3 +1,27 @@
+import React from 'react';
+import SliderInput from "./SliderInput";
+import {Slider} from "@mui/material";
+
+const SearchPage = () => {
+
+
+    return (
+        <div className={"search-page"}>
+            <div className={"search-left"}>
+                <div className={"search-input"}>
+                    <input type={"search"} value={"Введите аниме.."}/>
+                </div>
+                <div className={"search-content"}>Контент</div>
+            </div>
+            <div className={"search-right"}>
+                <div className={"search-categories"}>
+                    <Slider
+                        getAriaLabel={() => 'Temperature range'}
+                        min={1}
+                        max={10}
+                        valueLabelDisplay="auto"
+                        getAriaValueText={(value, index) => "asd"}
+                        color="primary"
 
                     />
                 </div>
